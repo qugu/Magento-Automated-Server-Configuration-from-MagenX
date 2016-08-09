eecef63fff18600af4ea76b25e5c22aa2b5b0bc9
magenx <magenx@users.noreply.github.com>
2016-08-09 18:08:56 +0100

    Update MASC-M-7-v2.sh

--- Magento-2/CentOS-7/MASC-M-7-v2.sh
+++ Magento-2/CentOS-7/MASC-M-7-v2.sh
@@ -1489,10 +1489,10 @@ echo
 echo
 GREENTXT "Installation of Oracle Java 8 JDK RPM:"
 cd /usr/local/src
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
-yum -y -q localinstall jdk-8u60-linux-x64.rpm
-export JAVA_HOME=/usr/java/jdk1.8.0_60/jre
-echo "export JAVA_HOME=/usr/java/jdk1.8.0_60/jre" > /etc/profile
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm"
+yum -y -q localinstall jdk-8u102-linux-x64.rpm
+export JAVA_HOME=/usr/java/jdk1.8.0_102/jre
+echo "export JAVA_HOME=/usr/java/jdk1.8.0_102/jre" > /etc/profile
 echo
 echo
 GREENTXT "Installation of Logstash:"
