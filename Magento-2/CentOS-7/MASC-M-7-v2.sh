06e70eb30591f8a2c5210c6546c244755404049d
MagenX <magenx@users.noreply.github.com>
2016-12-30 23:01:40 +0000

    Update MASC-M-7-v2.sh

--- Magento-2/CentOS-7/MASC-M-7-v2.sh
+++ Magento-2/CentOS-7/MASC-M-7-v2.sh
@@ -1592,6 +1592,9 @@ echo
 curl -s -o n98-magerun2.phar https://files.magerun.net/n98-magerun2.phar
 chmod u+x bin/magento
 rm -rf CHANGELOG.md CONTRIBUTING.md COPYING.txt ISSUE_TEMPLATE.md LICENSE.txt LICENSE_AFL.txt nginx.conf.sample php.ini.sample
+GREENTXT "SAVING COMPOSER JSON AND LOCK"
+cp composer.json ../composer.json.saved
+cp composer.lock ../composer.lock.saved
 fi
 echo
 GREENTXT "FIXING PERMISSIONS"
